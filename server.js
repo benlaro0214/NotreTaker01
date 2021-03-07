85e65c919416656579088acfc9ce640ddc81d210
Ben Larocque <benlaro0214@hotmail.com>
2021-03-07 16:29:59 -0500

    changing ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteJSON = require('./db/db.json');
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
